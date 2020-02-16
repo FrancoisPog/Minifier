b184aeecaee3e3ffcd814cc229e12d8fef73f19e
FrancoisPog <francoispoguet@gmail.com>
2020-02-16 21:41:42 +0100

    copier directory is done

--- minifier.sh
+++ minifier.sh
@@ -32,24 +32,24 @@ match_pattern(){
 
 # Function cheking if a variable is already initialized
 # $1 : The variable name 
-isSet(){
+isSet()(
   ! test "$1" = ""
   return $?
-}
+)
 
 # Function assigning the value '1' to variables with the same name as the given parameter
 # $1 : The option (ex : css, f ...) without '-' or '--'
 # $2 : 'double' if the original option is of type '--', else if is of type '-'
 # Exemple : 
-#           set_option f -> will create $F and $F=1
-#           set_option css double -> will create $CSS and $CSS=1
-#           set_option css or set_option t double -> error
+#           'set_option f' -> will create $F and $F=1
+#           'set_option css double' -> will create $CSS and $CSS=1
+#           'set_option css' or 'set_option t double' -> error
 set_option(){
   # check option validity
   if test "$2" = 'double'; then
-    ! match_pattern "$1" '(^css$|^html$)' && { echo "The '--$1' option is not supported\n$USAGE" >&2 && exit 1; }                 # ===> EXIT : Wrong '--' option 
+    ! match_pattern "$1" '(^css$|^html$)' && { echo "The '--$1' option is not supported\n$USAGE" >&2 && exit 1; }       # ===> EXIT : Wrong '--' option 
   else
-    ! match_pattern "$1" '^[vft]$' && { echo "The '-$1' option is not supported\n$USAGE" >&2 && exit 1; }                         # ===> EXIT : Wrong '-' option
+    ! match_pattern "$1" '^[vft]$' && { echo "The '-$1' option is not supported\n$USAGE" >&2 && exit 1; }        # ===> EXIT : Wrong '-' option
   fi
 
   # Create the variable associated with the option and check if it's the first time
@@ -76,7 +76,7 @@ check_arguments(){
   
   if test $# -eq 1 && test "$1" = "--help"; then
     help
-    exit 0                                                                                                                        # ===> EXIT : after help message
+    exit 0                                 # ===> EXIT : after help message
   fi
 
   ARG_NB=0 
@@ -87,7 +87,7 @@ check_arguments(){
     OPT="$OPT"
     test $ARG_NB -eq $TAGS_FILE_INDEX && continue # skip the argument just after the '-t' option
 
-    test $OPT = '--help' && { echo "The '--help' option must be alone\n$USAGE" >&2 && exit 1; }                                   # ===> EXIT : '--help' isn't alone
+    test $OPT = '--help' && { echo "The '--help' option must be alone\n$USAGE" >&2 && exit 1; }             # ===> EXIT : '--help' isn't alone
   
 
     # check '-' options
@@ -97,9 +97,8 @@ check_arguments(){
       if match_pattern $OPT 't' ;then
         TAGS_FILE_INDEX=$(($ARG_NB+1))
         TAGS_FILE=$(eval echo \$$TAGS_FILE_INDEX)
-        test -f "$TAGS_FILE" || { echo "Error : '-t' : Invalid tags_file '$TAGS_FILE'\n$USAGE">&2 && exit 3; }                    # ===> EXIT : Invalid tags_file
+        test -f "$TAGS_FILE" || { echo "Error : '-t' : Invalid tags_file '$TAGS_FILE'\n$USAGE">&2 && exit 3; }           # ===> EXIT : Invalid tags_file
         TAGS_BLOCK=$(cat $TAGS_FILE)
-        echo $TAGS_BLOCK
       fi
 
       continue
@@ -107,13 +106,13 @@ check_arguments(){
 
     # check '--' options
     if match_pattern $OPT "^--" ; then
-      OPT=$(echo $OPT | sed -r -e 's/^--//');
+      OPT=$(echo $OPT | sed -r -e 's/^--//'); # deleting '--'
       set_option $OPT double
       continue
     fi
 
     if ! isSet $SRC_DIR ;then
-      test -d "$OPT" || { echo "Invalid path to dir_sources : '$OPT'\n$USAGE" >&2 && exit 4 ;  }                                  # ===> EXIT : Invalid source directory
+      test -d "$OPT" || { echo "Invalid path to dir_sources : '$OPT'\n$USAGE" >&2 && exit 4 ;  }                  # ===> EXIT : Invalid source directory
       SRC_DIR="$OPT" 
       continue
     fi
@@ -124,19 +123,19 @@ check_arguments(){
       continue
     fi
     
-    echo "Invalid argument '$OPT'\n$USAGE" >&2 && exit 5                                                                          # ===> EXIT : Invalid argument
+    echo "Invalid argument '$OPT'\n$USAGE" >&2 && exit 5                  # ===> EXIT : Invalid argument
 
   done
 
   if ! isSet $SRC_DIR ||  ! isSet $DEST_DIR ; then
     echo "Paths to 'dir_sources' and 'dir_dest' must be specified\n$USAGE" >&2
-    exit 3                                                                                                                        # ===> EXIT : source or destination directory not specified
+    exit 3                                                                                      # ===> EXIT : source or destination directory not specified
   fi
 
 }
 
 
-search_file(){
+execute_minifier(){
   if isSet $HTML && test -f "$1" && match_pattern "$1" "^.+\.html$"; then
     echo "- html file : $1"
     minifier_html "$1"
@@ -149,11 +148,9 @@ search_file(){
 
   # if the argument isn't directory, we stop the function
   test -d "$1" || return 
-  echo "$DEST_DIR"/"$1"
-  echo $( echo "$DEST_DIR"/"$1" | sed -r -e "s/\$DEST_DIR/\$SRC_DIR/g" )
   local I
   for I in "$1"/*; do
-      search_file "$I"
+      execute_minifier "$I"
   done
 }
 
@@ -161,48 +158,50 @@ search_file(){
 minifier_html(){
   SVG_IFS=$IFS
   # echo "$1"
-  cat "$1" | tr "\n" " " | sed -E -e 's/<!--[^(-->)]*-->//g'  -e 's/\t/ /g' -e 's/\r/ /g' -e 's/ +/ /g' -e 's/<([[:alpha:]]+ *)/<\L\1/g' -e 's/\/([[:alpha:]]+ *>)/\/\L\1/g' | tee "$DEST_DIR/$1" >/dev/null
+  cat "$1" | tr "\n" " " | sed -E -e 's/<!--([^(-->)])*-->/ /g'  -e 's/\t/ /g' -e 's/\r/ /g' -e 's/ +/ /g' -e 's/<([[:alpha:]]+ *)/<\L\1/g' -e 's/\/([[:alpha:]]+ *>)/\/\L\1/g' | tee "$1" >/dev/null
   
   if isSet $T ; then 
     IFS=':'
     for TAG in $TAGS_BLOCK; do
       TAG=$(echo "$TAG" | sed -e 's/\(.*\)/\L\1/')
-      FILE=$(cat "$DEST_DIR/$1")
-      echo "$FILE" | sed -E -e "s/ ?<$TAG> ?/<$TAG>/g"  -e "s/ <$TAG([^>]*)> /<$TAG\1>/g" -e "s/ ?<\/$TAG ?> ?/<\/$TAG>/g"  | tee "$DEST_DIR/$1" > /dev/null
-      ! test -s "$DEST_DIR"/"$1" && echo $TAG && exit 8
+      FILE=$(cat "$1")
+      echo "$FILE" | sed -E -e "s/ ?<$TAG> ?/<$TAG>/g"  -e "s/ <$TAG([^>]*)> /<$TAG\1>/g" -e "s/ ?<\/$TAG ?> ?/<\/$TAG>/g"  | tee "$1" > /dev/null
+      ! test -s "$1" && echo $TAG && exit 8
     done
   fi
   IFS=$SVG_IFS
 }
 
 minifier_css(){
-  cat "$1" | tr "\n" " " | sed -r  -e 's/ +/ /g' -e 's/\t/ /g' -e 's/\r/ /g' | tee "$DEST_DIR"/"$1" >/dev/null
+  cat "$1" | tr "\n" " " | sed -r  -e 's/ +/ /g' -e 's/\t/ /g' -e 's/\r/ /g' -e "s/\/\*[^*]*\*+([^\/*][^*]*\*+)*\// /g" | tee "$1" >/dev/null
 }
 
 
 copy_directory(){
-
-  test -d "$1" || return
-  
-  for I in "$1"/*; do
-    if test -f "$I" || test -d "$I" ; then 
-      echo $I
-      
-    fi
-    copy_directory $I
-  done
+  if test -d "$2" && ! isSet $F ;then
+    read -p "The '$2' directory already exist, would you overwrite it ? [Y/N] : " CHOICE
+    test "$CHOICE" = "Y" || test "$CHOICE" = "y" || { echo "Execution canceled, end of program."; exit 4;}   # ===> EXIT : User don't want to overwrite
+    rm -rf ./"$2" || { echo "Error :  overwrite cancelation"; exit 4;}
+  fi 
+  if isSet $F; then 
+    rm -rf ./"$2"  || { echo "Error :  overwrite cancelation"; exit 4;}
+  fi
+ 
+  cp -r "$1" ./"$2" || { echo "Error : dest_dir creation canceled"; exit 4;}
 }
 
 
-# MAIN
+#######################################################
+#                       MAIN                          #
+#######################################################
 
 USAGE='Enter "./minifier.sh --help" for more informations.'
 
 check_arguments "$@"
 
-# copy_directory $SRC_DIR $DEST_DIR
+copy_directory "$SRC_DIR" "$DEST_DIR"
 
-search_file $SRC_DIR 
+execute_minifier "$DEST_DIR"
 
 echo "\n"
 echo $"F:$F"
